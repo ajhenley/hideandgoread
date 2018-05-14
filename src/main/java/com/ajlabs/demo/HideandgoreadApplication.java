935fcb269f82a82692d5bfdb1eccd5d9355704d2
AJ Henley <ajhenley@gmail.com>
2018-05-14 05:04:33 -0400

    Initial commit

---
+++ src/main/java/com/ajlabs/demo/HideandgoreadApplication.java
@@ -0,0 +1,12 @@
+package com.ajlabs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideandgoreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HideandgoreadApplication.class, args);
+    }
+}
